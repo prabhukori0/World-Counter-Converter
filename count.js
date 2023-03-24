3f8551f0f1aa5770f0a56d2876377bc094f20fb0
prabhukori0 <128037834+prabhukori0@users.noreply.github.com>
2023-03-24 11:31:13 +0530

    Update count.js

--- count.js
+++ count.js
@@ -1,12 +1,11 @@
-// {/* <h1>World Counter and Typing Text</h1>
-//     <div id="container">
-//         <textarea id="text" placeholder="This is Enter Text"></textarea>
-//         <p>Count : <spam>0</spam> Words and <spam>0</spam> Characters</p> */}
-
 var texts = document.getElementById("textbox");
 texts.addEventListener ("input", function(e){
     let text = this.value.length;
    document.getElementById("char").innerHTML= text;
+   // Give this let variable. 
+   // First properties is trim().  Do you frist element and last element text types in the value but trim() properties not show element
+   // Senond time is split(" "). This properties text inside space between all space through in tha value
+   // Third time is filter. This peroperties is not type text in value 'Zero'
     let words = this.value.trim().split(" ").filter(function(elm){
         return elm != "";
     }).length;
