d9ed124746968484fbe481784e5c9d0fbac1ddf7
prabhukori0 <128037834+prabhukori0@users.noreply.github.com>
2023-03-27 11:47:49 +0530

    Update count.js

--- count.js
+++ count.js
@@ -1,15 +1,24 @@
+let text;
+let words;
 var texts = document.getElementById("textbox");
 texts.addEventListener ("input", function(e){
-    let text = this.value.length;
-   document.getElementById("char").innerHTML= text;
+   text = this.value.length;
+   document.getElementById("char").innerText= text;
    // Give this let variable. 
    // First properties is trim().  Do you frist element and last element text types in the value but trim() properties not show element
    // Senond time is split(" "). This properties text inside space between all space through in tha value
    // Third time is filter. This peroperties is not type text in value 'Zero'
-    let words = this.value.trim().split(" ").filter(function(elm){
+    words = this.value.trim().split(" ").filter(function(elm){
         return elm != "";
     }).length;
-    console.log(words);
-    document.getElementById("word").innerHTML = words;
-    
+    // console.log(words);
+    document.getElementById("word").innerText = words;    
 })
+let btnclear = document.getElementById("clear");
+btnclear.addEventListener ("click", function(e){
+    text=0;
+    words=0;
+    document.getElementById("char").innerText= text;
+    document.getElementById("word").innerText = words; 
+}
+
